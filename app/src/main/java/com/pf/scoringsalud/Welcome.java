78bef38fd61fb81cd7d4556f75075e0c45fd349d
jeesssik <jeesssik@gmail.com>
2020-09-30 23:03:43 -0300

    linkeo pantalla welcome>Auth

--- app/src/main/java/com/pf/scoringsalud/Welcome.java
+++ app/src/main/java/com/pf/scoringsalud/Welcome.java
@@ -1,13 +1,32 @@
 package com.pf.scoringsalud;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        button= findViewById((R.id.buttonW));
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                openAuthView();
+            }
+        });
+    }
+
+    public void openAuthView(){
+        Intent intent= new Intent(this, AuthActivity.class);
+        startActivity(intent);
     }
 }
