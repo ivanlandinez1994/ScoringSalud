f9aad0360b973c5004de66450f9c97d094bbfa19
jeesssik <jeesssik@gmail.com>
2020-09-30 22:38:56 -0300

    cambio de esquema de colores, agregado de pantalla de welcome

---
+++ app/src/main/java/com/pf/scoringsalud/Welcome.java
@@ -0,0 +1,13 @@
+package com.pf.scoringsalud;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
