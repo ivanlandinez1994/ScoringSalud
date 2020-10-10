13b991a67df93b031e8a19adfe369c5b74786a40
jeesssik <jeesssik@gmail.com>
2020-10-09 23:42:18 -0300

    Inicio contenidos y estilos pantalla Hidratación [no contador, no back]

--- app/src/main/java/com/pf/scoringsalud/Water.java
+++ app/src/main/java/com/pf/scoringsalud/Water.java
@@ -2,13 +2,27 @@ package com.pf.scoringsalud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class Water extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water);
+
+
+
+
+
     }
+
+
+
+
 }
