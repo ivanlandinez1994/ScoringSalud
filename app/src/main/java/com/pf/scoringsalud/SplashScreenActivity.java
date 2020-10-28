53a31ca7f84c90cf88f5228d20ac80e24c3bc63a
lcsalt <96.laltamirano@gmail.com>
2020-10-28 20:18:42 -0300

    [add] Splash Screen

--- app/src/main/java/com/pf/scoringsalud/SplashScreenActivity.java
+++ app/src/main/java/com/pf/scoringsalud/SplashScreenActivity.java
@@ -2,13 +2,30 @@ package com.pf.scoringsalud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class activity_splash_screen extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int DURATION = 3000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashScreenActivity.this, AuthActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURATION);
     }
 }
