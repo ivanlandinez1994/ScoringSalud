714f40426d220c2a0c2f87f333d940642812a356
ivan <ivan.landinez.gallego@gmail.com>
2020-11-04 22:05:53 -0300

    correccion manifest

--- app/src/main/java/com/pf/scoringsalud/api/Config.java
+++ app/src/main/java/com/pf/scoringsalud/api/Config.java
@@ -2,7 +2,7 @@ package com.pf.scoringsalud.api;
 
 public abstract class Config {
 
-    public static final String URL_API="http://192.168.0.26:8081/";
+    public static final String URL_API="http://192.168.0.89:8081/";
 
 }
 
